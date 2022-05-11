780f45c5d2579fd724cd226605430f3385bf5c6b
Trainy32 <cjstkdthsu98@naver.com>
2022-05-11 12:31:29 +0900

    코멘트 연동중 : db.diary에 연결점 만듬. 점점 쌓이게 바꾸어야함!

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify, request, jsonify, redirect, url_for
 app = Flask(__name__)
 
+from datetime import datetime
+
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.kdc5v.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta_pjt_mypicDiary_test
@@ -15,7 +17,7 @@ def main():
 def diary_view(page_num):
     diary_data = db.diary.find_one({'diary_num':int(page_num)})
 
-    return render_template('diary.html', diary_data = diary_data)
+    return render_template('diary.html', page_num = page_num, diary_data = diary_data)
 
 
 ## 코멘트 API
@@ -25,23 +27,31 @@ def show_comment():
 
     return jsonify({'all_comment': comments})
 
+
 @app.route('/diary/comment', methods=['POST'])
 def save_comment():
     comment_receive = request.form['comment_give']
+    diary_num_receive = request.form['diary_num_give']
 
     comment_count = list(db.comment.find({}, {'_id': False}))
     comment_num = len(comment_count) + 1
+    writtenTime = datetime.now()
 
-    doc = {
+    newComment = {
         'comment_num':comment_num,
         'comment': comment_receive,
-        'name' : '임시이름 ',
-        'comment_date' : '2022-05-10'
+        'name' : '임시이름',
+        'comment_date' : writtenTime.strftime('%Y-%m-%d  %H:%M')
     }
 
-    db.comment.insert_one(doc)
+    # old_comment = list(db.diary.find({}, {'_id': False})
+    # allComment = .update(newComment)
+
+    db.diary.update_one({'diary_num':int(diary_num_receive)},{'$set':{'comment':newComment}})
+    db.comment.insert_one(newComment)
 
     return jsonify({'msg': '코멘트 등록 완료!'})
+
 
 ## 일기추천 API
 @app.route('/diary/recommend', methods=['GET'])
